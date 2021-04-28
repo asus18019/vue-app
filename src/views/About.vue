801cedd4dedf089042f557e89e55b1832f0b7e86
asus18019 <72703431+asus18019@users.noreply.github.com>
2021-04-28 20:23:02 +0300

    First commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,109 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <input
+        v-model="inputValue"
+    />
+    <h3>Привет {{ setText }}</h3>
+    <hr/>
+    <button
+        @click="fetchDogs"
+    >Fetch dogs</button>
+    <button @click="clearBreeds">Clear breeds</button>
+    <div v-for="breed in breeds">
+      <p>{{ breed.name }}</p>
+      <p class="red"
+         v-if="breed.subBreeds.length"
+         v-for="subBreed in breed.subBreeds"
+      >{{ subBreed }}</p>
+    </div>
+    <hr/>
+    <div>
+      <button
+          @click="randomBreed"
+      >Random breed</button>
+      <div v-for="breed in randomBreedVar">
+        <p>{{ breed.name }}</p>
+        <p class="red"
+           v-if="breed.subBreeds.length"
+           v-for="subBreed in breed.subBreeds"
+        >{{ subBreed }}</p>
+      </div>
+    </div>
+  </div>
+  <hr/>
+  <div>
+    <button
+      @click="addRandom"
+    >Add random breed</button>
+    <button
+        @click="deleteRandomBreed"
+    >Delete random breed</button>
+    <div
+        v-for="breed in randomBreeds"
+    >{{breed.name}}</div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: '',
+      breeds: [],
+      randomBreedVar: [],
+      randomBreeds: []
+    }
+  },
+  methods: {
+    convert(data){
+      return Object.entries(data).map(item => ({name: item[0], subBreeds: item[1]}))
+    },
+    fetchDogs(){
+      fetch('https://dog.ceo/api/breeds/list/all')
+          .then(response => response.json())
+          .then(json => this.breeds = this.convert(json.message))
+    },
+    randomBreed(){
+      fetch('https://dog.ceo/api/breeds/list/all/random')
+        .then(response => response.json())
+        .then(json => this.randomBreedVar = this.convert(json.message))
+    },
+    clearBreeds(){
+      this.breeds = []
+    },
+    addRandom(){
+      fetch('https://dog.ceo/api/breeds/list/all/random')
+          .then(response => response.json())
+          .then(json => this.randomBreeds = this.randomBreeds.concat(this.convert(json.message)))
+    },
+    randomNumberGenerator(max){
+      return Math.floor(Math.random() * max)
+    },
+    deleteRandomBreed(){
+      let number = this.randomNumberGenerator(this.randomBreeds.length)
+      this.randomBreeds = [
+        ...this.randomBreeds.slice(0, number),
+        ...this.randomBreeds.slice(number + 1)
+      ];
+    }
+  },
+  computed: {
+    setText(){
+      if(this.inputValue.trim()){
+        return this.inputValue
+      } else {
+        return "незнакомец"
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+  button {
+    margin: 20px;
+  }
+</style>
